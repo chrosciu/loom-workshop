ef943e5d379619dd488e15256f7b6eb6fee5a0c5
Marcin Chrost <mchrost82@gmail.com>
2023-12-06 09:03:10 +0100

    Scoped value rebinding

---
+++ snippets/src/main/java/eu/chrost/scopedval/S01Rebinding.java
@@ -0,0 +1,24 @@
+package eu.chrost.scopedval;
+
+public class S01Rebinding {
+    private static final ScopedValue<String> X = ScopedValue.newInstance();
+    public static void main(String[] args) {
+        foo();
+    }
+
+    static void foo() {
+        //System.out.println(X.get());
+        ScopedValue.where(X, "hello").run(S01Rebinding::bar);
+        //System.out.println(X.get());
+    }
+
+    static void bar() {
+        System.out.println(X.get());
+        ScopedValue.where(X, "goodbye").run(S01Rebinding::baz);
+        System.out.println(X.get());
+    }
+
+    static void baz() {
+        System.out.println(X.get());
+    }
+}
