d25828080c951c6ab18a7f1fad54b426a8190eb8
Marcin Chrost <mchrost82@gmail.com>
2023-12-06 08:20:14 +0100

    Simple MDC with ThreadLocal (not working in forked tasks)

--- booking/src/main/java/eu/chrost/booking/BookingService.java
+++ booking/src/main/java/eu/chrost/booking/BookingService.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.Supplier;
@@ -25,21 +26,26 @@ public class BookingService {
     */
     private final ThreadFactory threadFactory = Thread.ofVirtual().name("booking-", 0).factory();
 
+    private final ThreadLocal<String> requestId = new ThreadLocal<>();
+
     @SneakyThrows
     public String book(String destination) {
+        requestId.set(UUID.randomUUID().toString());
         try (var scope = new StructuredTaskScope.ShutdownOnFailure("Booking", threadFactory)) {
             Supplier<String> there = scope.fork(() -> book(destination, THERE));
             Supplier<String> back = scope.fork(() -> book(destination, BACK));
             scope.join().throwIfFailed();
             return String.join("\n",there.get(), back.get());
+        } finally {
+            requestId.remove();
         }
     }
 
     @SneakyThrows
     private String book(String destination, TripType tripType) {
-        log.info("[{} {}] Booking start", destination, tripType);
+        log.info("[{} {} {}] Booking start", requestId.get(), destination, tripType);
         Thread.sleep(3000);
-        log.info("[{} {}] Booking end", destination, tripType);
+        log.info("[{} {} {}] Booking end", requestId.get(), destination, tripType);
         return String.format("Booked %s travel to:  %s", tripType, destination);
     }
 }
