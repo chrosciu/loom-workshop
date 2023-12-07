97a8f14bb9c87ec297d3ad4325095af2e1013100
Marcin Chrost <mchrost82@gmail.com>
2023-12-07 19:03:31 +0100

    Back and there travel

--- booking/src/main/java/eu/chrost/booking/BookingService.java
+++ booking/src/main/java/eu/chrost/booking/BookingService.java
@@ -4,14 +4,30 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import static eu.chrost.booking.BookingService.TripType.BACK;
+import static eu.chrost.booking.BookingService.TripType.THERE;
+
 @Service
 @Slf4j
 public class BookingService {
-    @SneakyThrows
+    enum TripType {
+        THERE,
+        BACK
+    }
+
     public String book(String destination) {
-        log.info("[{}] Booking start", destination);
+        return String.join("\n",
+                book(destination, THERE),
+                book(destination, BACK)
+        );
+    }
+
+    @SneakyThrows
+    private String book(String destination, TripType tripType) {
+        log.info("[{} {}] Booking start", destination, tripType);
         Thread.sleep(3000);
-        log.info("[{}] Booking end", destination);
-        return "Booked travel to: " + destination;
+        log.info("[{} {}] Booking end", destination, tripType);
+        return String.format("Booked %s travel to:  %s", tripType, destination);
     }
 }
+
