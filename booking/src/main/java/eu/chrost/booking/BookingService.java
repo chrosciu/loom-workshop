cd31cc81b635a8009d99c3a19cbdffc454e0bd5c
Marcin Chrost <mchrost82@gmail.com>
2023-12-07 20:41:44 +0100

    ScopedValue for storing request id

--- booking/src/main/java/eu/chrost/booking/BookingService.java
+++ booking/src/main/java/eu/chrost/booking/BookingService.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.Supplier;
@@ -21,21 +22,26 @@ public class BookingService {
 
     private final ThreadFactory threadFactory = Thread.ofVirtual().name("booking-", 0).factory();
 
+    private final ScopedValue<String> requestId = ScopedValue.newInstance();
+
     @SneakyThrows
     public String book(String destination) {
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure("Booking", threadFactory)) {
-            Supplier<String> there = scope.fork(() -> book(destination, THERE));
-            Supplier<String> back = scope.fork(() -> book(destination, BACK));
-            scope.join().throwIfFailed();
-            return String.join("\n",there.get(), back.get());
-        }
+        return ScopedValue.where(requestId, UUID.randomUUID().toString()).call(() -> {
+            try (var scope = new StructuredTaskScope.ShutdownOnFailure("Booking", threadFactory)) {
+                log.info("[{}] Starting booking", requestId.get());
+                Supplier<String> there = scope.fork(() -> book(destination, THERE));
+                Supplier<String> back = scope.fork(() -> book(destination, BACK));
+                scope.join().throwIfFailed();
+                return String.join("\n",there.get(), back.get());
+            }
+        });
     }
 
     @SneakyThrows
     private String book(String destination, TripType tripType) {
-        log.info("[{} {}] Booking start", destination, tripType);
+        log.info("[{} {} {}] Booking start", requestId.get(), destination, tripType);
         Thread.sleep(3000);
-        log.info("[{} {}] Booking end", destination, tripType);
+        log.info("[{} {} {}] Booking end", requestId.get(), destination, tripType);
         return String.format("Booked %s travel to:  %s", tripType, destination);
     }
 }
