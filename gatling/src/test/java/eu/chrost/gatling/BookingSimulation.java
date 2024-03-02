4d25356b5f724fc4171040c227dde6516df3d8ed
Marcin Chrost <mchrost82@gmail.com>
2024-03-02 10:17:34 +0100

    Enabled virtual threads in Tomcat

--- gatling/src/test/java/eu/chrost/gatling/BookingSimulation.java
+++ gatling/src/test/java/eu/chrost/gatling/BookingSimulation.java
@@ -1,16 +1,17 @@
 package eu.chrost.gatling;
 
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static io.gatling.javaapi.core.CoreDsl.exec;
 import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
-
-import io.gatling.javaapi.core.ChainBuilder;
-import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Simulation;
-import io.gatling.javaapi.http.HttpProtocolBuilder;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class BookingSimulation extends Simulation {
 
